48e9850231016ae4d36b4918c5f2b94dbed55de8
ShyRobin <shy_robin@163.com>
2023-04-13 19:09:42 +0800

    feat: pyim

--- packages.el
+++ packages.el
@@ -10,6 +10,9 @@
 ;(package! some-package)
 (package! key-chord)
 
+(package! pyim)
+(package! pyim-basedict)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
