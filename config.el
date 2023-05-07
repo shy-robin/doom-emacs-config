03c233e376e26c6f8b8d9d2c9641b1a403596565
ShyRobin <shy_robin@163.com>
2023-05-07 13:39:19 +0800

    fix: treemacs font

--- config.el
+++ config.el
@@ -28,7 +28,10 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "SauceCodePro Nerd Font" :size 16))
+(setq doom-font (font-spec :family "SauceCodePro Nerd Font" :size 15))
+
+;; treemacs font
+(setq doom-variable-pitch-font (font-spec :family "SauceCodePro Nerd Font" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
