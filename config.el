14524015de4403670e98eaee1dbbcb3f204088e3
ShyRobin <shy_robin@163.com>
2023-06-11 15:56:34 +0800

    feat: not overwrite clipboard

--- config.el
+++ config.el
@@ -229,3 +229,8 @@
   (setq doom-modeline-persp-name t))
 
 (setq +treemacs-git-mode 'deferred)
+
+;; 删除不影响剪贴板
+(defun bb/evil-delete (orig-fn beg end &optional type _ &rest args)
+    (apply orig-fn beg end type ?_ args))
+(advice-add 'evil-delete :around 'bb/evil-delete)
